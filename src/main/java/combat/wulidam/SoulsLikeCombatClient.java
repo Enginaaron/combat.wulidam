9d74acbce7e3167e174dbe831faee6fc06d402c2
Howu1124 <Howardwu1124@gmail.com>
2026-05-01 13:37:21 -0400

    tutorial changes

---
+++ src/main/java/combat/wulidam/SoulsLikeCombatClient.java
@@ -0,0 +1,10 @@
+package combat.wulidam;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulsLikeCombatClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
